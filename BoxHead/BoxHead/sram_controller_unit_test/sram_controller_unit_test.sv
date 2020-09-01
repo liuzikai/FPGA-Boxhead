447683d2bd00534453950920e981aafbf0b8d49f
liuzikai <liuzikai@163.com>
2020-09-01 11:03:18 +0800

    Add SRAM controller unit test.

--- BoxHead/BoxHead/sram_controller_unit_test/sram_controller_unit_test.sv
+++ BoxHead/BoxHead/sram_controller_unit_test/sram_controller_unit_test.sv
@@ -1,5 +1,6 @@
 module sram_controller_unit_test (
     input  logic        CLOCK_50,
+    input  logic [3:0]  KEY,
 
     output logic        SRAM_CE, 
                         SRAM_UB, 
@@ -19,7 +20,57 @@ module sram_controller_unit_test (
                         VGA_HS,       // VGA horizontal sync signal
 );
 
-    logic vga_pll;
+    // ================================ Common ================================
+
+    // Alias 50MHz clock
+    logic clk;
+    assign clk = CLOCK_50;
+
+    // Synchronized reset
+    logic reset;
+    always_ff @ (posedge clk) begin
+        reset <= ~(KEY[0]);  // the push buttons are active low
+    end
+
+    // ================================ VGA ================================
+
+    // Setup VGA clock
+    logic vga_clk;
+    vga_pll vga_pll (
+        .inclk0(clk), 
+        .c0(vga_clk)
+    );
+
+    // Setup VGA controller
+    logic [9:0] vga_x, vga_y;
+    vga_controller vga_controller (.*);
+
+    logic frame_clk;
+    assign frame_clk = ~VGA_VS;
+
+    // Setup color extend
+    logic [15:0] vga_data;
+    vga_color_extend vga_color_extend (.*);
+
+    // ================================ SRAM ================================
+
+    // Setup SRAM clock
+    logic sram_clk;
+    sram_pll sram_pll (
+        .inclk0(clk), 
+        .c0(sram_clk)
+    );
+
+    // TEST: fixed program coordinates and data
+    logic [9:0] program_x, program_y;
+    logic [15:0] program_data, background_data;
+    assign program_x = 100;
+    assign program_y = 200;
+    assign program_data = 16'b11111000000000000;
+    assign background_data = 16'h0000;
+
+    // SRAM controller
+    sram_controller sram_controller (.*);
 
 
 endmodule
